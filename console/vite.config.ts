38cc3499e7513691ed5848944e92228fe2314497
WIN-20251217XRB\Administrator <zzh630725@163.com>
2026-03-05 20:10:39 +0800

    no message

--- console/vite.config.ts
+++ console/vite.config.ts
@@ -33,6 +33,12 @@ export default defineConfig(({ mode }) => {
     server: {
       host: "0.0.0.0",
       port: 5173,
+      proxy: {
+        "/api": {
+          target: "http://127.0.0.1:8088",
+          changeOrigin: true,
+        },
+      },
     },
     optimizeDeps: {
       include: ["diff"],
